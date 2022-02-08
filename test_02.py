197ba1544b79b085e402c0f037f9c427d39aa00b
mpenning <mike@pennington.net>
2022-02-07 23:58:30 -0600

    Remove old test script

--- test_02.py
+++ test_02.py
@@ -1,13 +1,19 @@
 from getpass import getuser, getpass
+
+from loguru import logger
+from cannon.main import account_factory
 from cannon import Shell, Account
 
-print("Logging into 172.16.1.3")
-acct = Account(getuser(), getpass())
-conn = Shell('172.16.1.3', credentials=(acct,), inventory="~/netactuate_inventory.ini", log_screen=True, debug=False)
-conn.execute('term len 0')
-conn.execute('show version')
-FOO = conn.response
+@logger.catch(default=True)
+def main():
+    print("Logging into 172.16.1.3")
+    acct = account_factory()
+    conn = Shell(host='172.16.1.3', account=acct, inventory="", debug=0)
+    conn.execute('term len 0')
+    conn.execute('show version')
+    print("OUTPUT", conn.response)
 
-conn.execute('show runn', timeout=2)
-config = conn.response
-print(FOO, "FOO")
+    conn.execute('show runn', timeout=2)
+    config = conn.response
+
+main()
